46aeeca41b940759ec168bcff535e7b707246947
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2012-12-23 23:03:52 +0100

    add model tests, not working

--- tests/model.js
+++ tests/model.js
@@ -1,8 +1,24 @@
 module("Model");
 
+
+
+//initialize
+//attribute
+//collection
+//id && id attribute
+//isNew
+//defaultAttributes
+//url
+//sync
+//validate
+//fetch
+//save
+//destroy
+//toJSON
 test( "a basic test example", function() {
   ok( true, "this test is fine" );
 });
+
 test( "a basic test example 2", function() {
   ok( true, "this test is fine" );
 });
