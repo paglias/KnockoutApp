507dfe834352938fd23b5e4b9cfc4ff2fbe2ffe9
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2013-01-01 17:22:09 +0100

    error if missing url, comments rewritten

--- src/sync.js
+++ src/sync.js
@@ -5,7 +5,7 @@
   //
   // - a model or a collection
   //
-  // Using the third, optional, parameter you can pass options to it (in that case options for the Ajax call)
+  // You can pass options to it using the third parameter (in that case the options will be passed to the ajax call)
   KnockoutApp.Sync = function(method, model, _options){
 
     // Ensure jQuery is loaded
@@ -16,12 +16,16 @@
 
     params.dataType = 'json';
 
+    // On error response calls KnockoutApp.Utils.wrapError
     params.error = function(){
-      Utils.wrapError(arguments);
+      KnockoutApp.Utils.wrapError(arguments);
     };
 
     //Get the url of the model/collection (model.url or model.url())
     params.url = Utils.unwrapValue(model, 'url');
+
+    // If params.url isn't defined throw an error
+    if(!params.url) throw "Url property must be defined in model/collection when using KnockoutApp.Sync";
 
     switch(method){
       case 'fetch':
@@ -31,6 +35,7 @@
         params.type = 'POST';
         if(model.name){
           params.data = {};
+          // If model.name property is set wrap model.toJSON() in an object model.name
           params.data[model.name] = model.toJSON();
         }else{
           params.data = model.toJSON();
@@ -40,6 +45,7 @@
         params.type = 'PUT';
         if(model.name){
           params.data = {};
+          // If model.name property is set wrap model.toJSON() in an object model.name
           params.data[model.name] = model.toJSON();
         }else{
           params.data = model.toJSON();
@@ -50,6 +56,6 @@
         break;
     }
 
-    // Make and return an Ajax call merging the *options* object passed as the third parameter with the *params* object
+    // Make and return an Ajax call merging the options object passed as the third parameter with the *params* object
     return $.ajax(ko.utils.extend(params, options));
   };
